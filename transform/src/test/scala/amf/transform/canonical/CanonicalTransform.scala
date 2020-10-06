8fbec21548a56f17d05fa41439e6b9425974d720
Tomas Fernandez <tfernandez@mulesoft.com>
2020-10-06 16:33:07 -0300

    Test: changed AmfCompiler for RuntimeCompiler. Also corrected the ErrorHandler for the Unhandled and had to change test files as consequence

--- transform/src/test/scala/amf/transform/canonical/CanonicalTransform.scala
+++ transform/src/test/scala/amf/transform/canonical/CanonicalTransform.scala
@@ -1,26 +1,40 @@
 package amf.transform.canonical
 
-import amf.client.parse.DefaultParserErrorHandler
 import amf.core.AMF
-import amf.core.remote.{Hint, RamlYamlHint}
+import amf.core.model.document.BaseUnit
+import amf.core.parser.errorhandler.UnhandledParserErrorHandler
+import amf.core.remote.Syntax.{Json, PlainText, Yaml}
+import amf.core.remote.{Cache, Context, Hint, RamlYamlHint}
+import amf.core.services.RuntimeCompiler
 import amf.core.unsafe.PlatformSecrets
-import amf.facades.{AMFCompiler, Validation}
-
-import scala.concurrent.ExecutionContext
+import amf.facades.Validation
+import amf.transform.canonical.CanonicalTransform.mediaType
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
-object CanonicalTransform extends CanonicalTransform
+object CanonicalTransform extends CanonicalTransform {
+
+  def mediaType(hint: Hint) = hint.syntax match {
+    case Yaml      => Some("application/yaml")
+    case Json      => Some("application/json")
+    case PlainText => Some("text/plain") // we cannot parse this?
+    case _         => None
+  }
+}
 
 trait CanonicalTransform extends PlatformSecrets {
 
   val CANONICAL_WEBAPI_DIALECT: String = "file://vocabulary/src/main/resources/dialects/canonical_webapi_spec.yaml"
 
-  def canonicalTransform(webApiPath: String, hint: Hint = RamlYamlHint, dialect: DialectRegistration = CanonicalDialectRegistration()) =
+  def canonicalTransform(webApiPath: String, hint: Hint = RamlYamlHint, dialect: DialectRegistration = CanonicalDialectRegistration()): Future[BaseUnit] = canonicalTransform(webApiPath, Some(hint), dialect)
+
+  def canonicalTransform(webApiPath: String, hint: Option[Hint], dialect: DialectRegistration): Future[BaseUnit] =
     for {
       _           <- AMF.init()
       _           <- Validation(platform)
-      unit        <- AMFCompiler(webApiPath, platform, hint, eh = DefaultParserErrorHandler()).build()
+//      unit        <- AMFCompiler(webApiPath, platform, hint, eh = DefaultParserErrorHandler()).build()
+      unit        <- RuntimeCompiler(webApiPath, mediaType = hint.flatMap(mediaType), hint.map(h => h.vendor.name), Context(platform), Cache(), errorHandler = UnhandledParserErrorHandler)
       _           <- dialect.registerDialect()
       transformed <- CanonicalWebAPISpecTransformer.transform(unit)
     } yield {
