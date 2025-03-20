e1e478cf37e89c1bf260f4a2d9567be1777b47e6
Loose <alejandro.loose@mulesoft.com>
2025-03-20 11:10:42 -0300

    W-10548359 - Adding OAS 3.1 related changes (Scopes/JsonSchemaDialect)

--- exporters/src/test/scala/amf/exporters/VocabularyPropertiesTest.scala
+++ exporters/src/test/scala/amf/exporters/VocabularyPropertiesTest.scala
@@ -26,7 +26,7 @@ class VocabularyPropertiesTest extends AsyncFunSuite with VocabularyTest {
   }
 
   test("Vocabularies parsing api_contract") {
-    testVocabulary(api_contract, 32, 50)
+    testVocabulary(api_contract, 32, 51)
   }
 
   test("Vocabularies parsing core") {
